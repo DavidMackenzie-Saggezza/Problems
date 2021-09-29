6699814e2f7d757872db3d56cdf6fb997fad1539
DavidMacKenzie <david.mackenzie@saggezza.com>
2021-09-29 12:54:27 +0100

    Slight improvement to subset problem. Now completes in less than a second up to 28 elements but still exponential...
    3: 3ms
    4: 0ms
    5: 0ms
    6: 0ms
    7: 0ms
    8: 0ms
    9: 0ms
    10: 0ms
    11: 0ms
    12: 0ms
    13: 0ms
    14: 0ms
    15: 1ms
    16: 0ms
    17: 1ms
    18: 2ms
    19: 3ms
    20: 7ms
    21: 8ms
    22: 13ms
    23: 35ms
    24: 63ms
    25: 101ms
    26: 185ms
    27: 388ms
    28: 680ms
    29: 1174ms
    30: 2307ms
    31: 4702ms
    32: 9417ms
    33: 19019ms
    34: 38653ms

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+
 import static Puzzle7.EqualSubsets.equalSubsets;
 
 public class Main {
     public static void main(String[] args) {
-        int[] list = {1,1,1,1,1,1,1,1,1,1,1};
-        System.out.println(equalSubsets(list));
+        for (int i = 3; i < 37; i++) {
+            int[] list = new int[i];
+            Arrays.fill(list, 1);
+            list[0] = i - 1;
+            long time = System.currentTimeMillis();
+            equalSubsets(list);
+            System.out.println(i + ": " + (System.currentTimeMillis() - time) + "ms");
+        }
     }
 }
