8c2da030dfb5ac75153cfab324c42f613adb5707
DavidMacKenzie <david.mackenzie@saggezza.com>
2021-09-29 15:05:50 +0100

    Added new solution to subset problem. Idea is similar except we iteratively add all possible sums from the back of the list and then check for a match at the end. Taken from https://www.youtube.com/watch?v=IsvocB5BJhw.

--- src/main/java/Puzzle7/EqualSubsets.java
+++ src/main/java/Puzzle7/EqualSubsets.java
@@ -1,12 +1,32 @@
 package Puzzle7;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class EqualSubsets {
     // This recursive approach "works" but will only complete in short time for lists with around 12 numbers or less.
     // If all cases are checked (as in MultipleSameElementAndSumReturnsTrue) then this will stop returning in reasonable time.
 
     public static boolean equalSubsets(int[] list) {
+        int total = Arrays.stream(list).sum();
+        if (total % 2 == 1) {
+            return false;
+        }
+        int targetSum = total / 2;
+        Set<Integer> nums = new HashSet<>();
+        for (int i = list.length - 1; i >= 0; i--) {
+            List<Integer> newNums = new ArrayList<>();
+            for (Integer num: nums) {
+                newNums.add(num + list[i]);
+            }
+            for (Integer num: newNums) {
+                nums.add(num);
+            }
+            nums.add(list[i]);
+        }
+        return nums.contains(targetSum);
+    }
+
+    public static boolean equalSubsetsOld(int[] list) {
         int total = Arrays.stream(list).sum();
         if (total % 2 == 1) {
             return false;
@@ -20,15 +40,19 @@ public class EqualSubsets {
             return list[0] == sum || list[1] == sum || list[0] + list[1] == sum;
         }
         for (int i = 0; i < list.length; i++) {
+            int[] leftArray = Arrays.copyOfRange(list,0, i);
             int[] rightArray;
             if (i != list.length - 1) {
-                rightArray = Arrays.copyOfRange(list,i + 1, list.length);
+                rightArray = Arrays.copyOfRange(list,i + 1, list.length - 1);
             }
             else {
                 rightArray = new int[0];
             }
+            int[] subArray = new int[list.length - 1];
+            System.arraycopy(leftArray, 0, subArray, 0, leftArray.length);
+            System.arraycopy(rightArray, 0, subArray, leftArray.length, rightArray.length);
 
-            if (sumInSet(rightArray, sum - list[i])) {
+            if (sumInSet(subArray, sum - list[i])) {
                 return true;
             }
         }
