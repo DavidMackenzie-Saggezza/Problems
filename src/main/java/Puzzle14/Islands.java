99f16d0f8338e18f2f5e418925a753e4b222ecd4
DavidMacKenzie <david.mackenzie@saggezza.com>
2021-10-05 16:53:07 +0100

    Slight refactor to Islands

--- src/main/java/Puzzle14/Islands.java
+++ src/main/java/Puzzle14/Islands.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Islands {
     public static int numIslands(int[][] matrix) {
+
         class Point {
             private int x;
             private int y;
@@ -38,36 +39,31 @@ public class Islands {
             }
         }
 
-        int height = matrix.length;
-        if (height == 0) {
+        if (matrix.length == 0) {
             return 0;
         }
-        int width = matrix[0].length;
-
 
         List<HashSet<Point>> islands = new ArrayList<>();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (matrix[i][j] == 1) {
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix[0].length; x++) {
+                if (matrix[y][x] == 1) {
                     HashSet<Point> newIsland = new HashSet<>();
-                    newIsland.add(new Point(j, i));
-                    for (int k = -1; k < 2; k++) {
-                        for (int l = -1; l < 2; l++) {
-                            if (!(k == 0 && l == 0)) {
-                                int x = j + l;
-                                int y = i + k;
-                                if (x >= 0 && x < width && y >= 0 && y < height) {
-                                    Point point = new Point(x, y);
-                                    List<HashSet<Point>> toBeDeleted = new ArrayList<>();
-                                    for (HashSet<Point> island: islands) {
-                                        if (island.contains(point)) {
-                                            newIsland.addAll(island);
-                                            toBeDeleted.add(island);
-                                        }
+                    newIsland.add(new Point(x, y));
+                    for (int yOffset = -1; yOffset < 2; yOffset++) {
+                        for (int xOffset = -1; xOffset < 2; xOffset++) {
+                            int xAdj = x + xOffset;
+                            int yAdj = y + yOffset;
+                            if (xAdj >= 0 && xAdj < matrix[0].length && yAdj >= 0 && yAdj < matrix.length && !(yOffset == 0 && xOffset == 0)) {
+                                Point pointAdj = new Point(xAdj, yAdj);
+                                List<HashSet<Point>> toBeDeleted = new ArrayList<>();
+                                for (HashSet<Point> island: islands) {
+                                    if (island.contains(pointAdj)) {
+                                        newIsland.addAll(island);
+                                        toBeDeleted.add(island);
                                     }
-                                    toBeDeleted.forEach(islands::remove);
                                 }
+                                toBeDeleted.forEach(islands::remove);
                             }
                         }
                     }
@@ -78,5 +74,3 @@ public class Islands {
         return islands.size();
     }
 }
-
-
