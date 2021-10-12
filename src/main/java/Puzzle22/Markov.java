aa1003e2d9abd6687aa16312baaa54fec5429c2c
DavidMacKenzie <david.mackenzie@saggezza.com>
2021-10-12 10:30:22 +0100

    Change cumulativeProb variable in Markov question

--- src/main/java/Puzzle22/Markov.java
+++ src/main/java/Puzzle22/Markov.java
@@ -9,13 +9,13 @@ public class Markov {
     public static HashMap<String, Integer> computeStatesVisited(List<Probability> transitionProbabilities, String startingState, int steps) {
         // No error checking, assuming that all probabilities from each state sum to 1.
         // Otherwise this may break.
-        HashMap<String, HashMap<String, Double>> probs = new HashMap<>();
+        HashMap<String, HashMap<String, Double>> cumulativeProbs = new HashMap<>();
         for (Probability probability: transitionProbabilities) {
-            if (!probs.containsKey(probability.getSourceState())) {
-                probs.put(probability.getSourceState(), new HashMap<>());
+            if (!cumulativeProbs.containsKey(probability.getSourceState())) {
+                cumulativeProbs.put(probability.getSourceState(), new HashMap<>());
             }
-            double cumulativeProb = probs.get(probability.getSourceState()).values().stream().reduce(0.0, Double::sum);
-            probs.get(probability.getSourceState()).put(probability.getDestinationState(), probability.getProbability() + cumulativeProb);
+            double cumulativeProb = cumulativeProbs.get(probability.getSourceState()).values().stream().reduce(0.0, Double::sum);
+            cumulativeProbs.get(probability.getSourceState()).put(probability.getDestinationState(), probability.getProbability() + cumulativeProb);
         }
 
         HashMap<String, Integer> statesVisited = new HashMap<>();
@@ -23,7 +23,7 @@ public class Markov {
         String state = startingState;
         for (int i = 0; i < steps; i++) {
             double randomProb = Math.random();
-            state = probs.get(state).entrySet()
+            state = cumulativeProbs.get(state).entrySet()
                     .stream()
                     .filter(j -> j.getValue() > randomProb)
                     .min(Comparator.comparingDouble(Map.Entry::getValue))
